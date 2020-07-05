9073358019be00aed262f67b1713932e6fff8796
Muhammadarif98 <marip55598@gmail.com>
2020-07-05 21:30:26 +0300

    dimens

---
+++ app/src/main/java/softdesign/com/ui/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package softdesign.com.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import softdesign.com.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
