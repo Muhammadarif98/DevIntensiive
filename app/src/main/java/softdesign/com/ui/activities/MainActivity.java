b2e9143208ee6d1f8cf49ce276ea623e1b44d6ef
Muhammadarif98 <marip55598@gmail.com>
2020-07-05 22:31:57 +0300

    l

--- app/src/main/java/softdesign/com/ui/activities/MainActivity.java
+++ app/src/main/java/softdesign/com/ui/activities/MainActivity.java
@@ -3,15 +3,17 @@ package softdesign.com.ui.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import softdesign.com.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG ="Main.Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d(TAG,"onCreate");
     }
 }
