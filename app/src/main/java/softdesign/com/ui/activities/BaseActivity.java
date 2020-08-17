39882de064a7ced94ad6503363c2d7e0893a4456
Muhammadarif98 <marip55598@gmail.com>
2020-08-17 23:29:46 +0300

    r

---
+++ app/src/main/java/softdesign/com/ui/activities/BaseActivity.java
@@ -0,0 +1,20 @@
+package softdesign.com.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class BaseActivity extends AppCompatActivity {
+
+    static final String TAG ="";
+
+    public void ShowProgress(){
+
+    }
+
+    public void HideProgress(){
+
+    }
+    public void ShowError(){
+
+    }
+}
